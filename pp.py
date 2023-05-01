3b54ca3dbf5a3fe9490294e69a3ee004a9808726
Ben Cavins <ben.cavins@flatironschool.com>
2023-05-01 13:01:17 -0400

    add student and generate_programming_pairs;

---
+++ pp.py
@@ -0,0 +1,12 @@
+import random
+from student import students
+
+
+def generate_programming_pairs():
+    random.shuffle(students)
+    for i in range(0, len(students), 2):
+        print(students[i].slack_handle, students[i+1].slack_handle)
+
+
+if __name__ == '__main__':
+    generate_programming_pairs()
---
+++ student.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, name, slack_handle=None):
+        self.name = name
+        self.slack_handle = slack_handle
+    
+    def __repr__(self) -> str:
+        return f"<Student {self.name}, {self.slack_handle}>"
+
+
+students = [
+    Student('Leif Enoksen', '@Leif Enoksen'),
+    Student('Chris Hontos', '@Chris Hontos'),
+    Student('Michael Ibragimchayev', '@Michael Ibragimchayev'),
+    Student('Samuel Peck', '@Sam Peck'),
+    Student('Jamal Portericker', '@Jamal Portericker'),
+    Student('Michoel Rivkin', '@Michoel'),
+    Student('Anglea S', '@Angela Shen'),
+    Student('Ryan Sinnott', '@Ryan Sinnott'),
+    Student('James Sullivan', '@James'),
+    Student('David Yarmush', '@David Yarmush'),
+]
